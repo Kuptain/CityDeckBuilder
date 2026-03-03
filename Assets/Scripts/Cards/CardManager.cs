53740321d756f6057ecfc83104bde20051045d44
Hildebrandt <philiphi@web.de>
2026-03-03 12:26:33 +0100

    added Deck infrastructure

---
+++ Assets/Scripts/Cards/CardManager.cs
@@ -0,0 +1,126 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    [Header("variables")]
+    public int HandSize;
+    [Header("listen")]
+    public List<Card> deck = new List<Card>(10);
+    public List<Card> hand;
+    public List<Card> discardedCards;
+    public void ShuffleDeck()
+    {
+        List<Card> newDeck = new List<Card>();
+        while ( deck.Count > 0)
+        {
+            int RandomIndex = Random.Range(0, deck.Count);
+            newDeck.Add(deck[RandomIndex]);
+            deck.RemoveAt(RandomIndex);
+        }
+        deck = newDeck;
+    }
+
+ 
+
+    public void DrawCards(int count)
+    {
+        for(int i = 0; i < count; i++)
+        {
+            DrawCard();
+        }
+    }
+
+
+    public void DrawCard(int index = 0)
+    {
+
+        if (deck.Count > 0)
+        {
+            index = Mathf.Clamp(index, 0, deck.Count - 1);
+            hand.Add(deck[index]);
+            deck.RemoveAt(index);
+        }
+        else
+        {
+            ReshuffleDiscard();
+            if (deck.Count > 0)
+            {
+                index = Mathf.Clamp(index, 0, deck.Count - 1);
+                hand.Add(deck[index]);
+                deck.RemoveAt(index);
+            }
+            else
+            {
+                //add reaction to nno cards beeing drawn
+                Debug.Log("No cards in the Deckk to Draw");
+            }
+        }
+    }
+
+    public void DiscardCard(int index = 0)
+    {
+        if(index<hand.Count && index >= 0)
+        {
+            discardedCards.Add(hand[index]);
+            hand.RemoveAt(index);
+        }
+    }
+    public void DiscardCard(Card card)
+    {
+        if (hand.Contains(card))
+        {
+            discardedCards.Add(card);
+            hand.Remove(card);
+        }
+    }
+
+    public void DiscardHand()
+    {
+        discardedCards.AddRange(hand);
+        hand.Clear();
+    }
+
+    public void ReshuffleDiscard()
+    {
+        deck.AddRange(discardedCards);
+        ShuffleDeck();
+        discardedCards.Clear();
+    }
+
+    #region test functions
+    [ContextMenu("shuffle Deck")]
+    public void Test_Shuffle()
+    {
+        ShuffleDeck();
+    }
+
+    [ContextMenu("resshuffle Discard")]
+    public void Test_Reshuffle()
+    {
+        ReshuffleDiscard();
+    }
+
+    [ContextMenu("draw Hand")]
+    public void Test_DrawHand()
+    {
+        DrawCards(HandSize);
+    }
+
+    [ContextMenu("draw Card")]
+    public void Test_Draw()
+    {
+        DrawCard();
+    }
+    [ContextMenu("discard Card")]
+    public void Discard()
+    {
+        DiscardCard(0);
+    }
+    [ContextMenu("discard Hand")]
+    public void Test_DiscardHand()
+    {
+        DiscardHand();
+    }
+    #endregion
+}
