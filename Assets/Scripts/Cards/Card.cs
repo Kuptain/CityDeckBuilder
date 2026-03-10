5f9e3b0c2c4d812ccc5d553b9795d23adf6de0d1
Daniel Schutko <danielschutko@gmail.com>
2026-03-11 00:49:30 +0100

    Update Card.cs

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -6,14 +6,5 @@ public class Card : ScriptableObject
 {
     [HideInInspector] public bool selected;
     public Sprite sprite;
-    public List<RessourceType> ressources;
-}
-
-public enum RessourceType
-{
-    none = 0,
-    wood = 1,
-    stone = 2,
-    gold = 3,
-    sheep = 4
+    public List<ResourceType> ressources;
 }
