d684c1695613740afb8d140278b6e3a1d77384c9
Hildebrandt <philiphi@web.de>
2026-03-11 11:25:36 +0100

    [fixed] combined scene

--- Assets/scriptableObjects/Buildings/BuildingData.cs
+++ Assets/scriptableObjects/Buildings/BuildingData.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 using System.Collections.Generic;
 using Mono.Cecil;
 
@@ -12,5 +13,16 @@ public class BuildingData : ScriptableObject
     public byte ID;
     public List<ResourceCost> resourceCosts;
     public List<Card> cardsToAdd;
+
+    public ResourceCost EffectCost;
+    public UnityEvent<BuildingData> OnBuild = new UnityEvent<BuildingData>();
+    public UnityEvent OnDrag = new UnityEvent();
+    public UnityEvent OnClick = new UnityEvent();
+    public UnityEvent OnEndOfRound = new UnityEvent();
+
+    public void createWater(BuildingData data)
+    {
+
+    }
 }
 
