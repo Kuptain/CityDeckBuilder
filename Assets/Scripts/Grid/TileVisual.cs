7852f839117dde695391e317227fddc4e57d9b80
Hildebrandt <philiphi@web.de>
2026-04-22 09:55:34 +0200

    [fixed] build error
    buildready

--- Assets/Scripts/Grid/TileVisual.cs
+++ Assets/Scripts/Grid/TileVisual.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
-using static UnityEditor.UIElements.ToolbarMenu;
 
 [System.Serializable]
 class TileDirectionOutline
